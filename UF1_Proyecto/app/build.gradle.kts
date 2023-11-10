aa0e701b7d074b048552dc090b4ee95990615eb4
Ruben Villar Pombo <a22rubenvp@iessanclemente.net>
2023-11-10 14:30:07 +0100

    Preparación proyecto

--- UF1_Proyecto/app/build.gradle.kts
+++ UF1_Proyecto/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.uf1_proyecto"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.uf1_proyecto"
