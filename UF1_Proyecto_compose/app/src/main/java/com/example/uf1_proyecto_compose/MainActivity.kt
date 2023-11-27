7835fa05c778694a3fbc66d993310ce0c73371cb
Ruben Villar Pombo <a22rubenvp@iessanclemente.net>
2023-11-27 14:30:43 +0100

    fin clase

--- UF1_Proyecto_compose/app/src/main/java/com/example/uf1_proyecto_compose/MainActivity.kt
+++ UF1_Proyecto_compose/app/src/main/java/com/example/uf1_proyecto_compose/MainActivity.kt
@@ -4,24 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.uf1_proyecto_compose.ui.theme.UF1_Proyecto_composeTheme
@@ -59,15 +65,18 @@ fun GreetingPreview() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home() {
+
 
     Column(
         verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.Start,
     ) {
-
+        var rs: Unit
         //TopBar
+        TopAppBar(title = { "Title" })
         Row(
             modifier = Modifier
                 .background(Color.Gray)
@@ -83,9 +92,52 @@ fun Home() {
                 .weight(1f)
                 .fillMaxSize()
         ) {
+            Column {
+                Row(
+                    modifier = Modifier
+                        .height(190.dp)
+                        .padding(24.dp, 16.dp, 24.dp, 0.dp)
+                        .fillMaxWidth()
+                        .align(CenterHorizontally)
+                ) {
+                    Row (modifier = Modifier
+                        .background(Color.Gray)
+                        .fillMaxSize()
+                        .padding(10.dp)
+                        , verticalAlignment = Alignment.Bottom
+                    ) {
+                        Text(text = "Title")
+
+
+                    }
+                }
+
+
+
+                Row(
+                    modifier = Modifier
+                        .height(190.dp)
+                        .padding(24.dp, 16.dp, 24.dp, 0.dp)
+                        .fillMaxWidth()
+                        .align(CenterHorizontally)
+                ) {
+                    Row (modifier = Modifier
+                        .background(Color.Gray)
+                        .fillMaxSize()
+                        .padding(10.dp)
+                        , verticalAlignment = Alignment.Bottom
+                    ) {
+                        Text(text = "Title")
+
+
+                    }
+                }
+            }
 
         }
-        
+
+
+
         Row(
             modifier = Modifier
                 .background(Color.Gray)
@@ -106,7 +158,7 @@ fun Home() {
                 }
 
                 Button(
-                    onClick = { /*TODO*/ },
+                    onClick = { },
                     modifier = Modifier
                         .width(70.dp)
                         .height(70.dp)
@@ -149,86 +201,10 @@ fun Home() {
 
 @Composable
 fun Player() {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterVertically),
-        horizontalAlignment = Alignment.Start,
-    ) {
-        //TODO:TopBar
-        Row(
-            modifier = Modifier
-                .background(Color.Gray)
-                .height(70.dp)
-                .fillMaxSize()
-        ) {
+    Text(text = "Player")
+}
 
-        }
-
-        //TODO: App Content (News)
-        Row(
-            modifier = Modifier
-                .background(Color.White)
-                .weight(1f)
-                .fillMaxSize()
-        ) {
-
-
-        }
-
-        //TODO: BottomBar
-        Row(
-            modifier = Modifier
-                .background(Color.Gray)
-                .height(70.dp)
-                .fillMaxSize()
-        ) {
-            Button(
-                onClick = { /*TODO*/ }, modifier = Modifier
-                    .width(70.dp)
-                    .height(70.dp)
-                    .weight(.5f)
-                    .fillMaxSize(),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                shape = RectangleShape
-            ) {
-                Text(text = "Noticias")
-            }
-
-            Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .width(70.dp)
-                    .height(70.dp)
-                    .weight(.5f)
-                    .fillMaxSize(),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                shape = RectangleShape
-            ) {
-                Text(text = "Player")
-            }
-
-            Button(
-                onClick = { /*TODO*/ }, modifier = Modifier
-                    .width(70.dp)
-                    .height(70.dp)
-                    .weight(.5f)
-                    .fillMaxSize(),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                shape = RectangleShape
-            ) {
-                Text(text = "Chat")
-            }
-
-            Button(
-                onClick = { /*TODO*/ }, modifier = Modifier
-                    .width(70.dp)
-                    .height(70.dp)
-                    .weight(.5f)
-                    .fillMaxSize(),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                shape = RectangleShape
-            ) {
-                Text(text = "Podcast")
-            }
-        }
-    }
+@Composable
+fun News() {
+    Text(text = "News")
 }
