47706ce471e2e7c1a82b8a8484d3aef1fa5d1873
Ruben Villar <a22rubenvp@iessanclemente.net>
2023-12-18 03:50:13 +0100

    Player logo

--- UF1_Proyecto_compose/app/src/main/java/com/example/uf1_proyecto_compose/ui/player/PlayerScreen.kt
+++ UF1_Proyecto_compose/app/src/main/java/com/example/uf1_proyecto_compose/ui/player/PlayerScreen.kt
@@ -2,46 +2,51 @@
 
 package com.example.uf1_proyecto_compose.ui.player
 
-import android.net.Uri
-import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.uf1_proyecto_compose.R
 import com.example.uf1_proyecto_compose.ui.components.PlayerBig
 import com.example.uf1_proyecto_compose.ui.components.qualitySelector
-import com.example.uf1_proyecto_compose.ui.utils.HIGH_QUALITY
-import com.example.uf1_proyecto_compose.ui.utils.LOW_QUALITY
-import com.example.uf1_proyecto_compose.ui.utils.MEDIUM_QUALITY
 import com.example.uf1_proyecto_compose.ui.utils.RadioPlayer
-import com.example.uf1_proyecto_compose.ui.utils.RadioPlayerHelper
-import com.example.uf1_proyecto_compose.ui.utils.play
-import com.example.uf1_proyecto_compose.ui.utils.quality
 
 @Composable
 fun PlayerScreen(viewModel: PlayerViewModel) {
     val player = RadioPlayer()
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier = Modifier.size(16.dp))
+        Spacer(modifier = Modifier.size(32.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.novahit_logo_full),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .scale(2f)
+        )
+        Spacer(modifier = Modifier.size(64.dp))
 
         qualitySelector()
 
-        Spacer(modifier = Modifier.size(16.dp))
+        Spacer(modifier = Modifier.weight(1f))
 
         PlayerBig()
+
+        Spacer(modifier = Modifier.weight(1f))
 
     }
 
